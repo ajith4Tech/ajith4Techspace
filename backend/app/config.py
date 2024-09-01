5047be25b64ff5460d51ae367672776c1b21da32
ajith4Tech <ajith@tech4goodcommunity.com>
2024-09-02 00:14:54 +0530

    Setting up backend for website

---
+++ backend/app/config.py
@@ -0,0 +1,4 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'mysql://website_user:Ajithbm@01@localhost/website_database'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = '121E5BE568C2BF4C734969DF64752'
