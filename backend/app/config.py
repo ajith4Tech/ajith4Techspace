3a475ca40afc303e91cac070a14e99a24a3fd956
ajith4Tech <ajith@tech4goodcommunity.com>
2024-09-02 00:31:00 +0530

    Created Config file after setting up database

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,4 +1,5 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql://website_user:Ajithbm@01@localhost/website_database'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://website_user:Ajithbm%4001@localhost/website_database'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = '121E5BE568C2BF4C734969DF64752'
+
