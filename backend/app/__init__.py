d76034ad66eb6599854b97d70c13f3b61da0b7f0
ajith4Tech <ajith@tech4goodcommunity.com>
2024-09-02 00:31:48 +0530

    Added init file to create flask app

---
+++ backend/app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from .config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import pymysql
+
+
+
+pymysql.install_as_MySQLdb()
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    return app
